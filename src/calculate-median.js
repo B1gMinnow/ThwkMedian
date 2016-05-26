6562b556e58033f86cef979765b623ac2a898a19
BigMinnow <b1gminnow@outlook.com>
2016-05-26 23:07:09 +0800

    try first

--- src/calculate-median.js
+++ src/calculate-median.js
@@ -1,6 +1,23 @@
 function calculate_median(arr) {
   //请勿改动函数名
-  var s = 'ss';
+  var array[];
+  for(var i = 0; i < arr.length; i++)
+  {
+  	if(i % 2 === 0)
+  		array.push(arr[i]);
+  }
+
+  array.sort(function(x,y){
+  	return x-y;
+
+  });
+
+  if(array.length % 2 === 0)
+  {
+  	return (array[array.length/2] + array[array.length/2 - 1])/2;
+  }
+  else
+  	return array[array.length/2];
 }
 
 module.exports = calculate_median;
